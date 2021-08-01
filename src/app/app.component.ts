c2a60a919e9948e0c4826b7dd301b8d9313887ab
TadasSa <t.sabaitis@gmail.com>
2021-08-01 12:49:36 +0300

    uzkomentuotas kas neveikia kad pasiruosti gynimui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 
@@ -9,7 +10,9 @@ import { EmployeeService } from './employee.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  public employees: Employee[];
+  public employees: Employee[] = [];
+
+  // public deleteEmployee: Employee;
 
   constructor(private employeeService: EmployeeService){}
 
@@ -29,7 +32,45 @@ export class AppComponent implements OnInit {
     );
   }
 
-  public onOpenModal(employee: Employee, mode: String): void {
+  public onAddEmployee(addForm: NgForm): void {
+    document.getElementById('add-employee-form')?.click();
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      },
+    );
+  }
+
+  public onUpdateEmployee(employee: Employee): void {
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  // public onDeleteEmloyee(employeeId: number): void {
+  //   this.employeeService.deleteEmployee(employeeId).subscribe(
+  //     (response: void) => {
+  //       console.log(response);
+  //       this.getEmployees();
+  //     },
+  //     (error: HttpErrorResponse) => {
+  //       alert(error.message);
+  //     }
+  //   );
+  // }
+
+
+  public onOpenModal(employee: Employee, mode: string): void {
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
     button.type = 'button';
@@ -42,6 +83,7 @@ export class AppComponent implements OnInit {
       button.setAttribute('data-target', '#updateEmployeeModal');
     }
     if (mode === 'delete') {
+      // this.deleteEmployee = employee;
       button.setAttribute('data-target', '#deleteEmployeeModal');
     }
     container?.appendChild(button);
